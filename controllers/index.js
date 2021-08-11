2d9dd485ede4143f8be9142df81d2b703e643c14
Brody Beebe <bbeebe13@gmail.com>
2021-08-11 14:03:49 -0600

    added read me, testing api

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api/');
-const homeRoutes = require('./api/home-routes');
+const homeRoutes = require('./home-routes');
 const dashboardRoutes = require('./dashboard-routes.js');
 
 router.use('/', homeRoutes);
