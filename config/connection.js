66f6e699c542c7d8295b05c6ce77838f54c35c76
Brody Beebe <bbeebe13@gmail.com>
2021-08-11 19:18:11 -0600

    added public folder

--- config/connection.js
+++ config/connection.js
@@ -19,7 +19,6 @@ if (process.env.JAWSDB_URL) {
 //   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
 //       host: 'localhost',
 //       dialect: 'mysql',
-//       port: 3306
 //       // dialectOptions: {
 //       //   decimalNumbers: true,
 //       // },
